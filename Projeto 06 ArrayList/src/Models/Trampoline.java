1b34a2f7de3c94b148b5dc8d8a68a53d90035df3
henricker <henriquevieira.siqxd@gmail.com>
2020-12-21 16:20:44 -0300

    Define lists with private modificator

--- Projeto 06 ArrayList/src/Models/Trampoline.java
+++ Projeto 06 ArrayList/src/Models/Trampoline.java
@@ -3,52 +3,52 @@ package Models;
 import java.util.ArrayList;
 
 public class Trampoline {
-    ArrayList <Kid> kidsWaiting = new ArrayList<Kid> ();
-    ArrayList <Kid> kidsPlaying = new ArrayList<Kid> ();
+    private ArrayList <Kid> kidsWaiting = new ArrayList<Kid> ();
+    private ArrayList <Kid> kidsPlaying = new ArrayList<Kid> ();
 
     public void arrive(Kid kid) {
         //Before add the kid in the first position of the kidsWaiting
-        kidsWaiting.add(0, kid);
+        this.kidsWaiting.add(0, kid);
     }
 
     public void in() { 
 
-        if(kidsWaiting.isEmpty()) {
+        if(this.kidsWaiting.isEmpty()) {
             System.out.println("Fail: Não existe nenhuma criança na fila de espera!");
             return;
         }
 
         //Take and remove a first kid in waiting queue
-        Kid firstKid = kidsWaiting.get(kidsWaiting.size() - 1);
-        kidsWaiting.remove(kidsWaiting.size() - 1);
+        Kid firstKid = this.kidsWaiting.get(this.kidsWaiting.size() - 1);
+        this.kidsWaiting.remove(this.kidsWaiting.size() - 1);
 
         //Now, add the fistkid in first position of the kidsPlayling
-        kidsPlaying.add(0, firstKid);
+        this.kidsPlaying.add(0, firstKid);
     }
 
     public void out() { 
 
-        if(kidsPlaying.isEmpty()) {
+        if(this.kidsPlaying.isEmpty()) {
             System.out.println("Fail: o pula pula já está vazio!");
             return;
         }
 
         //Take and remove a last kid in kidsPlaying
-        Kid lastKid = kidsPlaying.get(kidsPlaying.size() - 1);
-        kidsPlaying.remove(kidsPlaying.size() - 1);
+        Kid lastKid = kidsPlaying.get(this.kidsPlaying.size() - 1);
+        this.kidsPlaying.remove(this.kidsPlaying.size() - 1);
 
         //Now, add the laskid in first position of the waiting queue
-        kidsWaiting.add(0, lastKid);
+        this.kidsWaiting.add(0, lastKid);
     }
 
     public String toString() {
         String data = "";
-        for(Kid obj : kidsWaiting) {
+        for(Kid obj : this.kidsWaiting) {
             data += obj.toString();
         }
 
         data += "=> [ ";
-        for(Kid obj : kidsPlaying) {
+        for(Kid obj : this.kidsPlaying) {
             data += obj.toString();
         }
         data += "]";
